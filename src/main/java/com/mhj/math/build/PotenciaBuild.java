71421596c5c9a5507fd1cb14ae288fdf1d6528a2
t0404mnl <majara@STFALXC039460-W.stefanini.dom>
2018-04-30 13:49:19 -0300

    add build

---
+++ src/main/java/com/mhj/math/build/PotenciaBuild.java
@@ -0,0 +1,32 @@
+package com.mhj.math.build;
+
+import com.mhj.math.exception.BusinessException;
+import com.mhj.math.exception.RegraException;
+
+public class PotenciaBuild extends Build {
+
+	@Override
+	protected void validarParametros() throws BusinessException, RegraException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void titulo() throws BusinessException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void regras() throws BusinessException, RegraException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void resolucao() throws BusinessException, RegraException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
